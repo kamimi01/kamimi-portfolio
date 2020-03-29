1f01bbd3fa5d4f1dad448c64a38d320aa750e473
kamimi01 <awakaru0122@gmail.com>
2020-03-29 19:28:25 +0900

    add: sidebar

---
+++ js/home.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+(function() {
+  "use strict";
+
+  // 双方向データバインディング
+  var vm = new Vue({
+    el: "#app",
+    data: {
+      payload: {}
+    },
+    mounted: function() {
+      // データの初期化
+      this.initPage();
+    },
+    methods: {
+      initPage: function() {}
+    }
+  });
+})();
