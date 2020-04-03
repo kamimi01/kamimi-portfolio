a9b96968a7d92a0e7c71d28598fe872976e1222a
kamimi01 <awakaru0122@gmail.com>
2020-04-04 00:15:41 +0900

    add: tool-tip

--- js/home_dom.js
+++ js/home_dom.js
@@ -1,16 +1,16 @@
 $(function() {
   $(".copybtn").on("click", function() {
     let text = "xxxx@gmail.com";
-    // テキストエリアの作成
     let $textarea = $("<textarea></textarea>");
-    // テキストエリアに文章を挿入
+
     $textarea.text(text);
     $(this).append($textarea);
     $textarea.select();
-    // コピー
+
     document.execCommand("copy");
     $textarea.remove();
 
-    alert("Copy Mail Address to clipboard");
+    $('.tool-tip').fadeIn();
+    $(".tool-tip").fadeOut(2000);
   });
 });
